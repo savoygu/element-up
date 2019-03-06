0df117904e234a45bd7cee28a143f7994b28197b
savoygu <savoygu@gmail.com>
2019-03-06 14:44:00 +0800

    feat: Add carousel component

---
+++ packages/carousel/main.vue
@@ -0,0 +1,47 @@
+<template>
+  <el-carousel ref="elCarousel" class="up-carousel" v-bind="$attrs" v-on="$listeners">
+    <el-carousel-item v-for="(item, index) in data" v-bind="$attrs" v-on="$listeners" :key="index">
+      <slot v-bind="{ item, index }"></slot>
+    </el-carousel-item>
+  </el-carousel>
+</template>
+
+<script>
+import { Carousel, CarouselItem } from 'element-ui'
+
+export default {
+  name: 'UpCarousel',
+
+  components: {
+    [Carousel.name]: Carousel,
+    [CarouselItem.name]: CarouselItem
+  },
+
+  props: {
+    data: {
+      type: Array,
+      default: () => []
+    }
+  },
+
+  methods: {
+    setActiveItem (value) {
+      this.$refs.elCarousel.setActiveItem(value)
+    },
+    prev () {
+      this.$refs.elCarousel.prev()
+    },
+    next () {
+      this.$refs.elCarousel.next()
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@include b(up-carousel) {
+  @include e(item) {
+    //body
+  }
+}
+</style>
