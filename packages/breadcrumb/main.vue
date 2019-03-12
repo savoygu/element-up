45bf20788868f9ae0d697886bee85a20a0c7f750
savoygu <savoygu@gmail.com>
2019-03-12 16:13:22 +0800

    feat: Add breadcrumb component

---
+++ packages/breadcrumb/main.vue
@@ -0,0 +1,41 @@
+<template>
+  <el-breadcrumb v-bind="$attrs">
+    <el-breadcrumb-item
+      v-bind="{
+        ...$attrs,
+        ...(typeof item === 'string' ? {} : item)
+      }"
+      v-for="(item, index) in data"
+      :key="index"
+    >
+      <slot v-bind="{ item, index }">{{ typeof item === 'string' ? item: item[valueKey] }}</slot>
+    </el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
+
+<script>
+import { Breadcrumb, BreadcrumbItem } from 'element-ui'
+
+export default {
+  name: 'UpBreadcrumb',
+
+  components: {
+    [Breadcrumb.name]: Breadcrumb,
+    [BreadcrumbItem.name]: BreadcrumbItem
+  },
+
+  props: {
+    data: {
+      type: Array,
+      default: () => []
+    },
+    valueKey: {
+      type: String,
+      default: 'name'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/views/breadcrumb.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <up-breadcrumb :data="data"></up-breadcrumb>
+  </div>
+</template>
+
+<script>
+import UpBreadcrumb from 'packages/breadcrumb/main'
+
+export default {
+  name: 'Breadcrumb',
+
+  components: {
+    UpBreadcrumb
+  },
+
+  data () {
+    return {
+      data: [
+        { name: '首页', to: { path: '/' } },
+        { name: '活动管理', to: { path: '/' } },
+        { name: '活动列表', to: { path: '/' } },
+        { name: '活动详情' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
