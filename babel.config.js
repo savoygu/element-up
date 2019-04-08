93eeb4c3d19a0d6811291753f186d8ea44c5c096
savoygu <savoygu@gmail.com>
2019-04-08 15:21:18 +0800

    chore: update babel config

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,16 @@
 module.exports = {
-  presets: ['@vue/app', '@babel/preset-env'],
+  presets: [
+    [
+      '@vue/app',
+      {
+        loose: true,
+        modules: false,
+        targets: {
+          browsers: ['> 1%', 'last 2 versions', 'not ie <= 8']
+        }
+      }
+    ]
+  ],
   plugins: [
     [
       'component',
@@ -8,5 +19,32 @@ module.exports = {
         styleLibraryName: 'theme-chalk'
       }
     ]
-  ]
+  ],
+  env: {
+    utils: {
+      presets: [
+        [
+          '@vue/app',
+          {
+            loose: true,
+            modules: 'commonjs',
+            targets: {
+              browsers: ['> 1%', 'last 2 versions', 'not ie <= 8']
+            }
+          }
+        ]
+      ],
+      plugins: [
+        ['module-resolver', {
+          root: ['element-up'],
+          alias: {
+            'element-up/src': 'element-up/lib'
+          }
+        }]
+      ]
+    },
+    test: {
+      plugins: ['istanbul']
+    }
+  }
 }
