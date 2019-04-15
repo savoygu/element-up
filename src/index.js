9d0fbdee4c588738fe45f19079acf6a778849683
savoygu <savoygu@gmail.com>
2019-04-15 12:47:52 +0800

    feat: add locale support

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ import Dialog from '../packages/dialog/index.js'
 import FormItemRadio from '../packages/form-item-radio/index.js'
 import Popover from '../packages/popover/index.js'
 import Steps from '../packages/steps/index.js'
+import locale from 'element-up/src/locale'
 
 const components = [
   Breadcrumb,
@@ -49,9 +50,17 @@ const components = [
 ]
 
 const install = function (Vue, opts = {}) {
+  locale.use(opts.locale)
+  locale.i18n(opts.i18n)
+
   components.forEach(component => {
     Vue.component(component.name, component)
   })
+
+  Vue.prototype.$ELEMENT = {
+    size: opts.size || '',
+    zIndex: opts.zIndex || 2000
+  }
 
   Vue.prototype.$msg = Message
   Vue.prototype.$page = Page
@@ -63,6 +72,8 @@ if (typeof window !== 'undefined' && window.Vue) {
 
 export default {
   version: '0.3.0',
+  locale: locale.use,
+  i18n: locale.i18n,
   install,
   Breadcrumb,
   Dropdown,
---
+++ src/locale/index.js
@@ -0,0 +1,2 @@
+export * from 'element-ui/src/locale'
+export { default } from 'element-ui/src/locale'
