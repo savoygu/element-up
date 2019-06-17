b8f0aa4bbb7e2e088c7c71b1a16278694740922f
savoygu <savoygu@gmail.com>
2019-06-17 18:59:56 +0800

    feat: Add radio-group component [ci skip]

--- packages/radio/src/main.vue
+++ packages/radio/src/main.vue
@@ -1,66 +1,8 @@
-<template>
-  <el-radio-group
-    class="up-radio"
-    v-model="currentValue"
-    v-bind="$attrs"
-    v-on="{
-      ...$listeners,
-      change: (value) => $emit('up:radio-group:change', value)
-    }"
-    :size="size"
-    :disabled="disabled"
-  >
-    <component
-      :is="component"
-      v-bind="{
-        ...$attrs,
-        ...item
-      }"
-      v-on="{
-        ...$listeners,
-        change: (value) => $emit('up:radio:change', value, item)
-      }"
-      v-for="(item, index) in data"
-      :key="index"
-      :label="item[keyValue[1]]"
-    >{{item[keyValue[0]]}}</component>
-  </el-radio-group>
-</template>
-
 <script>
 import Radio from 'element-ui/packages/radio'
-import RadioButton from 'element-ui/packages/radio-button'
-import RadioGroup from 'element-ui/packages/radio-group'
-import createVModel from 'element-up/src/mixins/createVModel'
 
 export default {
-  name: 'UpRadio',
-
-  mixins: [
-    createVModel('currentValue')
-  ],
-
-  components: {
-    [RadioGroup.name]: RadioGroup,
-    [RadioButton.name]: RadioButton,
-    [Radio.name]: Radio
-  },
-
-  props: {
-    data: {
-      type: Array,
-      default: () => []
-    },
-    keyValue: {
-      type: Array,
-      default: () => ['name', 'value']
-    },
-    size: String,
-    disabled: Boolean,
-    component: {
-      type: String,
-      default: 'el-radio'
-    }
-  }
+  ...Radio,
+  name: 'UpRadio'
 }
 </script>
---
+++ packages/radio/src/radio-group.vue
@@ -0,0 +1,68 @@
+<template>
+  <el-radio-group
+    class="up-radio"
+    v-model="currentValue"
+    v-bind="$attrs"
+    v-on="{
+      ...$listeners,
+      change: (value) => $emit('up:radio-group:change', value)
+    }"
+    :size="size"
+    :disabled="disabled"
+  >
+    <component
+      :is="component"
+      v-bind="{
+        ...$attrs,
+        ...item
+      }"
+      v-on="{
+        ...$listeners,
+        change: (value) => $emit('up:radio:change', value, item)
+      }"
+      v-for="(item, index) in data"
+      :key="index"
+      :label="item[keyValue[1]]"
+    >
+      <slot name="up:item" :slot-scope="{ item, $index: index }">{{item[keyValue[0]]}}</slot>
+    </component>
+  </el-radio-group>
+</template>
+
+<script>
+import Radio from 'element-ui/packages/radio'
+import RadioButton from 'element-ui/packages/radio-button'
+import RadioGroup from 'element-ui/packages/radio-group'
+import createVModel from 'element-up/src/mixins/createVModel'
+
+export default {
+  name: 'UpRadioGroup',
+
+  mixins: [
+    createVModel('currentValue')
+  ],
+
+  components: {
+    [RadioGroup.name]: RadioGroup,
+    [RadioButton.name]: RadioButton,
+    [Radio.name]: Radio
+  },
+
+  props: {
+    data: {
+      type: Array,
+      default: () => []
+    },
+    keyValue: {
+      type: Array,
+      default: () => ['name', 'value']
+    },
+    size: String,
+    disabled: Boolean,
+    component: {
+      type: String,
+      default: 'el-radio'
+    }
+  }
+}
+</script>
