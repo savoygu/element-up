b381f975faa409aa62170f147204b96029987fcc
savoygu <savoygu@gmail.com>
2019-06-17 19:00:41 +0800

    feat: Add checkbox-group component [ci skip]

---
+++ packages/checkbox/src/checkbox-group.vue
@@ -0,0 +1,68 @@
+<template>
+  <el-checkbox-group
+    class="up-checkbox"
+    v-model="currentValue"
+    v-bind="$attrs"
+    v-on="{
+      ...$listeners,
+      change: (value) => $emit('up:checkbox-group:change', value)
+    }"
+    :size="size"
+    :disabled="disabled"
+  >
+    <component
+      :is="component"
+      v-bind="{
+        ...$attrs,
+        ...item
+      }"
+      v-on="{
+        ...$listeners,
+        change: (value) => $emit('up:checkbox:change', value, item)
+      }"
+      v-for="(item, index) in data"
+      :key="index"
+      :label="item[keyValue[1]]"
+    >
+      <slot name="up:item" :slot-scope="{ item, $index: index }">{{item[keyValue[0]]}}</slot>
+    </component>
+  </el-checkbox-group>
+</template>
+
+<script>
+import Checkbox from 'element-ui/packages/checkbox'
+import CheckboxButton from 'element-ui/packages/checkbox-button'
+import CheckboxGroup from 'element-ui/packages/checkbox-group'
+import createVModel from 'element-up/src/mixins/createVModel'
+
+export default {
+  name: 'UpCheckboxGroup',
+
+  mixins: [
+    createVModel('currentValue')
+  ],
+
+  components: {
+    [CheckboxGroup.name]: CheckboxGroup,
+    [CheckboxButton.name]: CheckboxButton,
+    [Checkbox.name]: Checkbox
+  },
+
+  props: {
+    data: {
+      type: Array,
+      default: () => []
+    },
+    keyValue: {
+      type: Array,
+      default: () => ['name', 'value']
+    },
+    size: String,
+    disabled: Boolean,
+    component: {
+      type: String,
+      default: 'el-checkbox'
+    }
+  }
+}
+</script>
--- packages/checkbox/src/main.vue
+++ packages/checkbox/src/main.vue
@@ -1,66 +1,8 @@
-<template>
-  <el-checkbox-group
-    class="up-checkbox"
-    v-model="currentValue"
-    v-bind="$attrs"
-    v-on="{
-      ...$listeners,
-      change: (value) => $emit('up:checkbox-group:change', value)
-    }"
-    :size="size"
-    :disabled="disabled"
-  >
-    <component
-      :is="component"
-      v-bind="{
-        ...$attrs,
-        ...item
-      }"
-      v-on="{
-        ...$listeners,
-        change: (value) => $emit('up:checkbox:change', value, item)
-      }"
-      v-for="(item, index) in data"
-      :key="index"
-      :label="item[keyValue[1]]"
-    >{{item[keyValue[0]]}}</component>
-  </el-checkbox-group>
-</template>
-
 <script>
 import Checkbox from 'element-ui/packages/checkbox'
-import CheckboxButton from 'element-ui/packages/checkbox-button'
-import CheckboxGroup from 'element-ui/packages/checkbox-group'
-import createVModel from 'element-up/src/mixins/createVModel'
 
 export default {
-  name: 'UpCheckbox',
-
-  mixins: [
-    createVModel('currentValue')
-  ],
-
-  components: {
-    [CheckboxGroup.name]: CheckboxGroup,
-    [CheckboxButton.name]: CheckboxButton,
-    [Checkbox.name]: Checkbox
-  },
-
-  props: {
-    data: {
-      type: Array,
-      default: () => []
-    },
-    keyValue: {
-      type: Array,
-      default: () => ['name', 'value']
-    },
-    size: String,
-    disabled: Boolean,
-    component: {
-      type: String,
-      default: 'el-checkbox'
-    }
-  }
+  ...Checkbox,
+  name: 'UpCheckbox'
 }
 </script>
