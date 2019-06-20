f7abab14d8dd34e33fa270c5f47888d5666609d2
savoygu <savoygu@gmail.com>
2019-06-20 10:03:47 +0800

    feat: Add default slot

--- packages/checkbox/src/checkbox-group.vue
+++ packages/checkbox/src/checkbox-group.vue
@@ -10,22 +10,24 @@
     :size="size"
     :disabled="disabled"
   >
-    <component
-      :is="component"
-      v-bind="{
-        ...$attrs,
-        ...item
-      }"
-      v-on="{
-        ...$listeners,
-        change: (value) => $emit('up:checkbox:change', value, item)
-      }"
-      v-for="(item, index) in data"
-      :key="index"
-      :label="item[keyValue[1]]"
-    >
-      <slot name="up:item" v-bind="{ item, $index: index }">{{item[keyValue[0]]}}</slot>
-    </component>
+    <slot>
+      <component
+        :is="component"
+        v-bind="{
+          ...$attrs,
+          ...item
+        }"
+        v-on="{
+          ...$listeners,
+          change: (value) => $emit('up:checkbox:change', value, item, index, data)
+        }"
+        v-for="(item, index) in data"
+        :key="index"
+        :label="item[keyValue[1]]"
+      >
+        <slot name="up:item" v-bind="{ item, $index: index }">{{item[keyValue[0]]}}</slot>
+      </component>
+    </slot>
   </el-checkbox-group>
 </template>
 
