2c73bb7aaef1b2412371e6d99574afd2dc8c2333
savoygu <savoygu@gmail.com>
2019-06-25 12:50:07 +0800

    refactor(progress): Update

--- packages/progress/src/main.vue
+++ packages/progress/src/main.vue
@@ -1,6 +1,6 @@
 <script>
 import Progress from 'element-ui/packages/progress'
-import { addClass } from 'element-ui/src/utils/dom'
+import { addClass, removeClass } from 'element-ui/src/utils/dom'
 
 export default {
   name: 'UpProgress',
@@ -21,6 +21,19 @@ export default {
       const reverse = this.reverse ? 0 : 1
       return `M 50 50 m 0 -${radius} a ${radius} ${radius} 0 1 ${reverse} 0 ${radius *
         2} a ${radius} ${radius} 0 1 ${reverse} 0 -${radius * 2}`
+    }
+  },
+
+  watch: {
+    striped (value) {
+      if (this.type !== 'line') return
+
+      const inner = this.$el.querySelector('.el-progress-bar__inner')
+      if (value) {
+        addClass(inner, 'up-progress-bar__striped')
+      } else {
+        removeClass(inner, 'up-progress-bar__striped')
+      }
     }
   },
 
