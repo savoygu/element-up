051fa505d01d88fb22ff7e8c0e607991b39dd0c2
savoygu <savoygu@gmail.com>
2019-08-09 11:35:13 +0800

    refactor: rewrite alert component

--- packages/alert/src/main.vue
+++ packages/alert/src/main.vue
@@ -1,30 +1,8 @@
 <script>
 import Alert from 'element-ui/packages/alert'
-import Component from 'element-up/packages/component'
 
 export default {
-  functional: true,
-
-  name: 'UpAlert',
-
-  components: {
-    [Alert.name]: Alert,
-    [Component.name]: Component
-  },
-
-  render (h, { data, listeners, slots }) {
-    return (
-      <up-component class="up-alert" component={Alert} {...{ attrs: data.attrs, on: listeners }}>
-        {slots().default}
-        {
-          slots().title && (
-            <template slot="title">
-              {slots().title}
-            </template>
-          )
-        }
-      </up-component>
-    )
-  }
+  ...Alert,
+  name: 'UpAlert'
 }
 </script>
